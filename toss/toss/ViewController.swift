fbc82b8d04efb6ee4e91883dc1a50ab5eaf73dda
choidam <37874238+choidam@users.noreply.github.com>
2022-07-11 22:47:21 +0900

    프로젝트 생성

---
+++ toss/toss/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  toss
+//
+//  Created by choidam on 2022/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+    }
+
+
+}
+
